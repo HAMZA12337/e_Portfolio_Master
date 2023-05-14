b89ff9f82cb7a6e1bf72792fb6b48e9cee9e562b
Your Name <you@example.com>
2023-05-14 17:52:10 +0100

    dded

--- components/Home/Reflections/Reflections.tsx
+++ components/Home/Reflections/Reflections.tsx
@@ -21,7 +21,7 @@ export default function Reflections() {
       Visit My Blog
       </span>
       <p className="flex font-Header tracking-wider text-gray-400 text-center px-6 sm:px-16 md:px-0 md:w-[600px]">
-      Welcome to my blog, where I share my thoughts and experiences on various topics ranging from personal growth, travel, and lifestyle to technology, entertainment, and current events. Through my writing, I aim to inspire and engage readers by offering insights, tips, and perspectives that are both informative and entertaining. Join me on this journey of self-discovery and exploration, and let's learn and grow together!
+      Welcome to my blog, where I share my thoughts and experiences on various topics ranging from personal growth, travel, and lifestyle to technology, entertainment, and current events. Through my writing, I aim to inspire and engage readers by offering insights, tips, and perspectives that are both informative and entertaining. Join me on this journey of self-discovery and exploration, and let s learn and grow together!
       </p>
       <div className="pt-4">
         <a href="https://braimi.blogspot.com/" target={"_blank"} rel="noreferrer">
